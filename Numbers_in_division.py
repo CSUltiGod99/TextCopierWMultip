98d15cb48c4ab2bf9e10d83bd95c4ce95ae7209f
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-01-01 03:15:59 +0000

    Add files via upload
    Divider Script for dividing the line when INPUTTING NUMBERS
    -
    -
       -
    -
    -
    -

---
+++ Numbers_in_division.py
@@ -0,0 +1,24 @@
+from math import *
+
+class Numbers_in_division:
+    def __init__(self):
+
+        Division_08 = 3197
+        Division_09 = 3198
+        Division_10 = 3200
+
+        print(Division_09 - Division_08)
+        print(Division_10)
+
+        Division_32 = 2
+        print(Division_09 + Division_32)
+
+
+
+
+
+
+
+
+
+
