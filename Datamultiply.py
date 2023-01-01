12e9a5ecb98f602bc9d0e79b875338f745b64bd4
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-01-01 03:13:26 +0000

    Add files via upload

---
+++ Datamultiply.py
@@ -0,0 +1,9 @@
+from math import *  #
+
+class Data_Collector:
+	def __init__(self):
+		data__06 = 33
+		data__07 = 3
+
+		print(33 * 2)
+		print(data__06 - data__07)
---
+++ main.py
@@ -0,0 +1,33 @@
+from Datamultiply import *
+from Numbers_in_division import *
+
+class main:
+
+    def __init__(self):
+        print("hi")
+
+        print(2)
+        print(3 * 3 * 3 * 3)
+        print(1 + 1)
+        print(3 * 12)
+        print(5 * 2)
+
+
+
+    def __init__(self, midrange, maxrange, year):
+        self.midrange = midrange
+        self.maxrange = maxrange
+        self.year = year
+
+    s.midrange
+
+
+
+    def __init__(self, disable, enable, offEnabler):
+        self.disable = disable
+        self.enable = enable
+        self.offEnabler = offEnabler
+
+
+
+
