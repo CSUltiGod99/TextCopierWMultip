2618cfc0bbce02c573caa466945f9293fcf47021
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-03-11 16:56:41 +0000

    Create Copy-Verify-Words.js

---
+++ Copy-Verify-Words.js
@@ -0,0 +1,32 @@
+
+
+namespace Copy-Verify-Words()
+{
+  class TextCopier
+
+      
+    public static void Verify(String[] args)
+    {
+        const word_Keys = ["Brian ", "Liam", "Nick", "Lesley"]; 
+        function copied_init()
+      {
+            // - Call on Copy Script: "text_copied_initializer.js"
+            for(int c = 0;)
+            {
+               console.log("Returning/name/text_copied_initializer.js[c++]\n");
+               return text_copied_initializer.js;
+            }
+      }
+      
+        function verifying()
+        {
+          if(word_Keys."Nick" == true)
+          {
+            console.log("This is the 3rd name on the list");
+          }
+          
+        }
+      
+      
+      
+
